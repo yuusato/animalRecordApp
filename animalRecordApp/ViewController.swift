e7c18e7e9c614153e3151c8692ab730632720fc5
yuu-sato <atohoney102@gmail.com>
2019-06-23 04:02:20 +0900

    既往歴・手術歴・入院歴記録

---
+++ animalRecordApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  animalRecordApp
+//
+//  Created by 佐藤結 on 2019/06/23.
+//  Copyright © 2019 佐藤結. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
